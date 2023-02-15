b8fb22ff3dd2c7a8bd1e0d3f5fafc9ee70c6d7ce
Pau Siquier <pausiqrey8@gmail.com>
2023-02-15 21:08:56 +0100

    Change Idioms

---
+++ js/translate.js
@@ -0,0 +1,11 @@
+var check=document.querySelector(".check");
+check.addEventListener('click',idioms);
+
+function idioms(){
+    let id=check.checked;
+    if(id==true){
+        location.href="es/index.html";
+    }else{
+        location.href="../index.html"; 
+    }
+}
